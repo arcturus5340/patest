8880ca0a76bba93b58a8cb1dd4b4dbb70a415269
arcturus5340 <arcturus5340@gmail.com>
2020-04-27 19:16:46 +0200

    Updated selectivity for selecting test methods

--- patest.pas
+++ patest.pas
@@ -1,23 +1,29 @@
 ﻿unit patest;
 
-type MainTestClass = class
+type TestCase = class
 end;
+
+
+//procedure assertEqual<T>(a, b: T);
+//begin
+//  println(a, b);
+//end;
+
 
 procedure main();
 begin
-  var subClassList := typeof(MainTestClass).Assembly.GetTypes().Where(t -> t.isSubClassof(typeof(MainTestClass)));
+  var subClassList := typeof(TestCase).Assembly.GetTypes().Where(t -> t.isSubClassof(typeof(TestCase)));
             
   foreach var subClass in subClassList do
     begin
     var subClassObj := System.Activator.CreateInstance(subClass);
     
     var objectClassMethods: array of string := ('$Init$', 'Equals', 'GetType', 'GetHashCode', 'ToString');
-    var methods := subClass.GetMethods.Where(m -> not objectClassMethods.Contains(m.Name));
+    var methods := subClass.GetMethods.Where(m -> m.Name.StartsWith('test'));
     
     foreach var method in methods do
-      begin
         method.Invoke(subClassObj, nil);
-      end;
+
     end;
   
 end;
