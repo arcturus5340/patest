0b6149683fdc795903e0ad7d3047074aecf1f3f4
arcturus5340 <arcturus5340@gmail.com>
2020-04-27 21:37:40 +0200

    Implemented more readable output

--- patest.pas
+++ patest.pas
@@ -1,36 +1,97 @@
 ﻿unit patest;
 
+var out_buff: List<string>;
+
+
+function _get_fail_msg(msg: string): string;
+begin
+  var stackTrace := new System.Diagnostics.StackTrace;
+  result := format('FAIL: {0} ({1}) '#10'    AssertionError: {2}', 
+                   stackTrace.GetFrame(2).GetMethod.Name, 
+                   stackTrace.GetFrame(2).GetMethod.DeclaringType, 
+                   msg);
+end;
+
+
 type TestCase = class
   procedure assertEqual<T>(a, b: T);
   begin
     if a = b then
-      println('Test passed!')
+      begin
+      write('.');
+      end
     else
-      println('Oops! Something get wrong..');
+      begin
+      write('F');
+      var msg := format('{0} <> {1}', a, b);
+      out_buff.Add(_get_fail_msg(msg));
+      end;
   end;
     
   procedure assertNotEqual<T>(a, b: T);
   begin
     if a <> b then
-      println('Test passed!')
+      begin
+      write('.');
+      end
     else
-      println('Oops! Something get wrong..');
+      begin
+      write('F');
+      var msg := format('{0} = {1}', a, b);
+      out_buff.Add(_get_fail_msg(msg));
+      end;
   end;
 
-  procedure assertTrue(cond: boolean);
+  procedure assertTrue<T>(a: T);
   begin
-    if cond then
-      println('Test passed!')
-    else
-      println('Oops! Something get wrong..');
+    try 
+      begin
+      var bool_a := System.Convert.ToBoolean(a);
+      if Bool_a then
+        begin
+        write('.');
+        end
+      else
+        begin
+        write('F');
+        var msg := 'Condition is not true';
+        out_buff.Add(_get_fail_msg(msg));
+        end;
+      end;
+    except
+      on Exception do
+        begin
+        write('F');
+        var msg := format('Параметр ({0}) не распознан как логическое значение', a);
+        out_buff.Add(_get_fail_msg(msg));
+        end;
+    end;    
   end;
 
-  procedure assertFalse(cond: boolean);
+  procedure assertFalse<T>(a: T);
   begin
-    if not cond then
-      println('Test passed!')
-    else
-      println('Oops! Something get wrong..');
+    try 
+      begin
+      var bool_a := System.Convert.ToBoolean(a);
+      if Bool_a then
+        begin
+        write('.');
+        end
+      else
+        begin
+        write('F');
+        var msg := 'Condition is not false';
+        out_buff.Add(_get_fail_msg(msg));
+        end;
+      end;
+    except
+      on Exception do
+        begin
+        write('F');
+        var msg := format('Параметр ({0}) не распознан как логическое значение', a);
+        out_buff.Add(_get_fail_msg(msg));
+        end;
+    end;
   end;
 
 end;
@@ -44,12 +105,23 @@ begin
     begin
     var subClassObj := System.Activator.CreateInstance(subClass);
     
+    var time := Milliseconds;
     var methods := subClass.GetMethods.Where(m -> m.Name.StartsWith('test'));
     foreach var method in methods do
-        method.Invoke(subClassObj, nil);
+      begin
+      method.Invoke(subClassObj, nil);
+      end;
+    writeln;
+    writeln('='*80);
+    foreach var fail in out_buff do
+      writeln(fail);
+    writeln('-'*80);
+    writeln(format('Ran {0} test in {1}s', methods.ToArray.Length, (Milliseconds-time)/1000));
     end;
   
 end;
 
+
 begin
+  out_buff := new List<string>;
 end.
